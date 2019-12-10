38d193a0ecf854c7bb5afab590c4544970779886
leihg <leihonggang@zhongruigroup.com>
2019-12-10 17:11:08 +0800

    remove configModule

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '../config/configModule';
-
 import { EntityValidationPipe } from './entityValidationPipe';
 
 @Module({
-  imports: [ConfigModule],
   providers: [
     EntityValidationPipe
   ],
-  exports: [ConfigModule]
 })
 export class SharedModule { }
