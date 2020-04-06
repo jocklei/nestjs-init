2583f10fc48e0c5f65a7e2d6c28873bf33bbeb91
jocklei <jocklei@aliyun.com>
2020-04-06 10:05:34 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger, INestApplication } from '@nestjs/common';
+import { Logger, INestApplication, ValidationPipe } from '@nestjs/common';
 
 import * as helmet from 'helmet';
 import * as compression from 'compression';
@@ -11,8 +11,15 @@ import { AuthInterceptor } from './core/authentication.interceptor';
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule, { cors: true });
 
+  const validationPipeOptions = new ValidationPipe({
+    whitelist: true, // 剥离属性
+    transform: true, // DTO类自动将有效负载转换为对象类型
+    disableErrorMessages: true, // 禁用返回验证详细错误信息
+  });
+
   app.use(helmet()); // helmet() 阻止跨站脚本攻击中间件
   app.use(compression()); // 压缩中间件
+  app.useGlobalPipes(validationPipeOptions);
 
   app.setGlobalPrefix('api/v1'); // setGlobalPrefix() 全局添加前缀
   app.useGlobalInterceptors(new AuthInterceptor()); // 认证拦截
