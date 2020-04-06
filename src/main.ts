c149deef53372008a440c2467198f4032fb97284
jocklei <jocklei@aliyun.com>
2020-04-06 10:44:50 +0800

    完善框架

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, INestApplication, ValidationPipe } from '@nestjs/common';
 
+import * as csurf from 'csurf';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
+import * as rateLimit from 'express-rate-limit';
 
 import { swagger } from './swagger';
 import { AppModule } from './app.module';
@@ -14,11 +16,20 @@ async function bootstrap() {
   const validationPipeOptions = new ValidationPipe({
     whitelist: true, // 剥离属性
     transform: true, // DTO类自动将有效负载转换为对象类型
-    disableErrorMessages: true, // 禁用返回验证详细错误信息
+    disableErrorMessages: true // 禁用返回验证详细错误信息
   });
 
-  app.use(helmet()); // helmet() 阻止跨站脚本攻击中间件
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'The system has detected that your request is illegal and has been blocked.'
+  });
+
+  app.use(csurf()); // 阻止跨站点请求伪造中间件
+  app.use(helmet()); // 阻止跨站脚本攻击中间件
   app.use(compression()); // 压缩中间件
+  app.use(limiter); // 限速暴力攻击
+
   app.useGlobalPipes(validationPipeOptions);
 
   app.setGlobalPrefix('api/v1'); // setGlobalPrefix() 全局添加前缀
